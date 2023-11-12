3c4a83c1b43e4a9fc5e6a452a05db50f60e051eb
Eduardo Fuentes <effs76@gmail.com>
2023-11-12 12:37:31 -0300

    404

---
+++ 404.php
@@ -0,0 +1,14 @@
+<div class="container-fluid px-100">
+    <div class="row py-100">
+        <div class="col-md-4 mx-auto">
+            <img src="assets/Group 206.png" alt="" class="mx-auto">
+        </div>
+        <div class="col-md-6 text-center">
+            <h1 class="font-solway color-azul">Ups! Error 404</h1>
+            <br>
+            <h3 class="font-kulim">No hemos encontrado la página que estás buscando.</h3>
+            <br>
+            <h3 class="font-kulim">Te recomendamos volver al inicio.</h3>
+        </div>
+    </div>
+</div>
