8fda744fa1d51905763965d5f5c7b9db8cdd2df2
Eduardo Fuentes <effs76@gmail.com>
2023-12-05 13:20:14 -0300

    Update servicios.php

--- servicios.php
+++ servicios.php
@@ -34,8 +34,13 @@ Template Name: Servicios
                         para potenciar las capacidades de sus hijos a través de los programas de atención.</p>
 
                         <br>
-                        <?php the_post_thumbnail('full', array('class' => 'mx-auto align-self-center text-center')); ?>
-                        <button type="button" class="border1 p-2 w-100 bg-boton hover-boton" data-bs-toggle="modal" data-bs-target="#modal1">
+                        
+                        
+                        
+                </div>
+                <div class="col-md-8 mx-auto">
+                    <?php the_post_thumbnail('full', array('class' => 'img-fluid')); ?>
+                    <button type="button" class="border1 p-2 bg-boton hover-boton" data-bs-toggle="modal" data-bs-target="#modal1">
                             Ver Línea de Tiempo
                         </button>
                 </div>
