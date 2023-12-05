b07a3d5f58ffc081a534abdc670e0485a3ff3ab9
Eduardo Fuentes <effs76@gmail.com>
2023-12-05 11:37:33 -0300

    Update servicios.php

--- servicios.php
+++ servicios.php
@@ -465,8 +465,8 @@ Template Name: Servicios
 <div class="container-fluid">
     <div class="row">
         <div class="col-md-12">
-            <div class="modal " tabindex="-1" id="modal1">
-                <div class="modal-dialog">
+            <div class="modal w-100" tabindex="-1" id="modal1">
+                <div class="modal-dialog w-100">
                     <div class="modal-content">
                     <div class="modal-header bg-color-beige bg-gradient">
                         <h5 class="modal-title  color-azul">Línea de Tiempo</h5>
@@ -478,10 +478,10 @@ Template Name: Servicios
 
                                 </div>
                                 <div class="col-md-10">
-                                    <h2 class="color-naranjo font-solway font-size-6">
+                                    <h2 class="color-azul font-solway font-size-6">
                                         Programa de Atención Temprana (0 a 6 años): Kinesiología - Educación – Fonoaudiología – Terapia Ocupacional
                                     </h2>
-                                    <h1>holas</h1>
+                                    
                                     <p class="font-size-3">
                                         Desde los 0 a 2  años trabajamos principalmente lo que son las habilidades motrices, considerando que generalmente los niños con S.D. , tienen hipotonía debemos estimular todas las áreas motoras, lo que les permitirá lograr en menor tiempo la marcha.
                                         Entre los  2 a 6 años se realiza estimulación cognitiva, en esta etapa se comienza con las primeras etapas de Lectura Global, facilitando así la inclusión escolar.  
