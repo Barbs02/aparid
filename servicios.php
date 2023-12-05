ea1b733390af9a5ae1d14e82544f32533a319b31
Eduardo Fuentes <effs76@gmail.com>
2023-12-05 11:54:02 -0300

    Update servicios.php

--- servicios.php
+++ servicios.php
@@ -478,11 +478,11 @@ Template Name: Servicios
 
                                 </div>
                                 <div class="col-md-10">
-                                    <h2 class="color-azul font-solway font-size-6">
+                                    <h2 class="color-azul font-solway font-size-5">
                                         Programa de Atención Temprana (0 a 6 años): Kinesiología - Educación – Fonoaudiología – Terapia Ocupacional
                                     </h2>
                                     
-                                    <p class="font-size-3">
+                                    <p class="font-size-2">
                                         Desde los 0 a 2  años trabajamos principalmente lo que son las habilidades motrices, considerando que generalmente los niños con S.D. , tienen hipotonía debemos estimular todas las áreas motoras, lo que les permitirá lograr en menor tiempo la marcha.
                                         Entre los  2 a 6 años se realiza estimulación cognitiva, en esta etapa se comienza con las primeras etapas de Lectura Global, facilitando así la inclusión escolar.  
                                     </p>
@@ -491,16 +491,7 @@ Template Name: Servicios
                                 <div class="col-md-2">
 
                                 </div>
-                                <div class="col-md-10">
-                                    <h2 class="color-azul font-solway font-size-6">
-                                        Programa de Atención Temprana (0 a 6 años): Kinesiología - Educación – Fonoaudiología – Terapia Ocupacional
-                                    </h2>
-                                    
-                                    <p class="font-size-3">
-                                        Desde los 0 a 2  años trabajamos principalmente lo que son las habilidades motrices, considerando que generalmente los niños con S.D. , tienen hipotonía debemos estimular todas las áreas motoras, lo que les permitirá lograr en menor tiempo la marcha.
-                                        Entre los  2 a 6 años se realiza estimulación cognitiva, en esta etapa se comienza con las primeras etapas de Lectura Global, facilitando así la inclusión escolar.  
-                                    </p>
-                                </div>
+                                
                             </div>
                         </div>
                         
