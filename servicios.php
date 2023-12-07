cdd0bb61f6cb31e0c829eedcce4c355e2f83e085
Eduardo Fuentes <effs76@gmail.com>
2023-12-07 10:52:21 -0300

    sss

--- servicios.php
+++ servicios.php
@@ -346,7 +346,7 @@ Template Name: Servicios
 
 <!-- -----------------------------------Modal-------------------------------------- -->
 
-<div class="container-fluid mx-auto">
+<div class="container-fluid mx-auto linea-vertical-container">
     <div class="row">
         <div class="col-md-12">
 
@@ -365,7 +365,9 @@ Template Name: Servicios
                             <div class="pt-3"></div>
                             <div class="container-fluid">
                                 <div class="row">
+            
                                     <div class="col-md-2">
+
                                     <?php
                                         $image = get_field('imagen_numero_metodologia');
                                         if (!empty($image)): ?>
