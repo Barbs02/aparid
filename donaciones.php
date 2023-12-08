301611245ee80dcf09dbf70550657f8e30684fd4
Eduardo Fuentes <effs76@gmail.com>
2023-12-08 17:31:39 -0300

    Update donaciones.php

--- donaciones.php
+++ donaciones.php
@@ -81,22 +81,7 @@ Template Name: Donaciones
     </div>
     <div class="container-fluid px-100">
     <div class="row ">
-                <div class="col-md-2 col-sm-4 align-self-center">
-                    <?php
-                    $image = get_field('imagen_trayecto_1');
-                    if (!empty($image)): ?>
-                        <img class="mx-auto img-fluid" src="<?php echo esc_url($image['url']); ?>"
-                        alt="<?php echo esc_attr($image['alt']); ?>" />
-                    <?php endif; ?>
-                </div>
-                <div class="col-md-10 col-sm-8 align-self-center">
-                    <h2 class="color-azul font-solway font-size-5">
-                        <?php the_field('titulo_paso_1'); ?>
-                    </h2>
-                    <h4 class="color-naranjo font-solway font-size-3">
-                        <?php the_field('descripcion_trayecto'); ?>
-                    </h4>
-                </div>
+                
                 <!-- <div class="col-md-2 col-sm-4 align-self-center">
                     <button type="button" class="border1 p-3 mx-auto bg-boton hover-boton color-azul font-size-3" data-bs-toggle="modal" data-bs-target="#modal1">
                         Ver Metodología
