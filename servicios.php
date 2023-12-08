a7ed39cbb401571357740c84a0214c1c355237df
Barbs02 <114110721+Barbs02@users.noreply.github.com>
2023-12-08 17:25:32 -0300

    c

--- servicios.php
+++ servicios.php
@@ -290,7 +290,7 @@ Template Name: Servicios
 
         <div class="container-fluid px-100 bg-color-beige pb-100 pt-4">
             <div class="row">
-                    <div class="col-md-4 mx-auto text-center" data-aos="fade-up" data-aos-duration="2000">
+                    <div class="col-md-4 mx-auto text-center" data-aos="fade-up" data-aos-duration="1000">
                         
                         <?php
                         $image = get_field('icono_referencial_objetivos_1');
@@ -302,7 +302,7 @@ Template Name: Servicios
                         <p class="font-size-1"><?php the_field('descripcion_objetivo_1'); ?></p>
                     </div>
 
-                    <div class="col-md-4 mx-auto text-center" data-aos="fade-up" data-aos-duration="2500">
+                    <div class="col-md-4 mx-auto text-center" data-aos="fade-up" data-aos-duration="2000">
                         
                         <?php
                         $image = get_field('icono_referencial_objetivos_2');
