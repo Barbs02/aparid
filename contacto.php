86d25a5d9f69a625d0b0d9c6d8646104d003fc57
Eduardo Fuentes <effs76@gmail.com>
2023-12-10 18:46:33 -0300

    Update contacto.php

--- contacto.php
+++ contacto.php
@@ -228,6 +228,7 @@ echo do_shortcode('[smartslider3 slider="2"]');
                     </div>
                 </div>
             </div>
+            <div class="col-md-2"></div>
             <div class="col-md-3">
                 <div class="row">
                     <div class="col-3">
