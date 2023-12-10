d167c9ae0b50b5956725f1e4e980eca50000c154
Eduardo Fuentes <effs76@gmail.com>
2023-12-10 17:28:17 -0300

    Update header.php

--- header.php
+++ header.php
@@ -17,8 +17,8 @@
 <?php $custom_logo = wp_get_attachment_image( get_theme_mod( 'custom_logo' ), '' ); ?>
 <nav class="navbar navbar-expand-md navbar-light bg-color-beige p-2 fixed-top">
     <div class="container-fluid d-flex align-items-center">
-    <a href="inicio logo-r">
-        <div class="logo ">
+    <a href="inicio">
+        <div class="logo logo-r">
             <?php echo $custom_logo ?>
         </div>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-menu" aria-controls="main-menu" aria-expanded="false" aria-label="Toggle navigation">
