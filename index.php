f8ebbc4d135c5bb87cdc4c02b1d02c464e05e216
Eduardo Fuentes <effs76@gmail.com>
2024-01-28 20:04:46 -0300

    Update index.php

--- index.php
+++ index.php
@@ -320,7 +320,7 @@ Template Name: Inicio
                 <p class="font-kulim font-size-2 font-bold"><?php the_field('nombre_del_hijo'); ?></p>
             <hr>
 
-                <div>
+                <div class="w-100">
                     <?php the_field('archivo-resena'); ?>
                     
                 </div>
