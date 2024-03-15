4d7487362fcff2dc141a65ea0d2f99ef9f439138
Eduardo Fuentes <effs76@gmail.com>
2024-03-15 12:57:21 -0300

    Update down.php

--- down.php
+++ down.php
@@ -391,7 +391,7 @@ Template Name: Sindrome de Down
             <?php endif; ?>
             <?php wp_reset_query(); ?>
             <div class="col-md-2 mx-auto py-100">
-                    <button class="border1 p-2 w-100 hover-boton bg-boton" href="<?php echo get_permalink( '15' ); ?>">Programas</button>
+                    <a class="border1 p-2 w-100 hover-boton bg-boton" href="<?php echo get_permalink( '15' ); ?>">Programas</a>
             </div>
         </div>
     </div>
