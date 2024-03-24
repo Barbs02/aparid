80455fe828f8060c14e2b28f79e597a1d93a77ce
Barbs02 <114110721+Barbs02@users.noreply.github.com>
2024-03-24 16:15:23 -0300

    prueba

--- prueba.php
+++ prueba.php
@@ -1,9 +1,22 @@
 <?php
-/*
-Template Name: Prueba
-*/
+     if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Recoger los datos del formulario
+        $nombre = $_POST['nombre'];
+        $apellido = $_POST['apellido'];
+        $celular = $_POST ['celular']
+        $email = $_POST['email'];
+        $mensaje = $_POST['mensaje'];
+    
+        $correo = 'suberobarbara14@gmail.com';
+        $asunto = 'Asunto del correo';
+        $mensaje = $nombre + $apellido + $celular +$email + $mensaje ;
+    
+        // Envío del correo
+        if (@mail($correo, $asunto, $mensaje)) {
+            echo 'El correo se ha enviado correctamente.';
+        } else {
+            echo 'Hubo un error al enviar el correo. Detalles del error: ' . error_get_last()['message'];
+    
+        }
+    }
 ?>
-<?php get_header(); ?>
-
-nnh
-<?php get_footer(); ?>
