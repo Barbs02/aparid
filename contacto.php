31fe0c559a1b0e61bcfc24d40e953ecbe10bc881
Eduardo Fuentes <effs76@gmail.com>
2024-03-24 16:40:12 -0300

    Update contacto.php

--- contacto.php
+++ contacto.php
@@ -264,6 +264,20 @@ echo do_shortcode('[smartslider3 slider="2"]');
                 <?php endif; ?>
                 <?php wp_reset_query(); ?>
 </div>
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script> <!-- AOS JS -->
+<script>
+    AOS.init();
+
+</script>
+
+
+</body>
+</html>
+
 <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Recoger los datos del formulario
@@ -285,16 +299,4 @@ echo do_shortcode('[smartslider3 slider="2"]');
     
         }
     }
-    ?>
-
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script> <!-- AOS JS -->
-<script>
-    AOS.init();
-
-</script>
-
-
-</body>
-</html>
+    ?>  
